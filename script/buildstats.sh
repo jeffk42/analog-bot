76d52f453dc29ab52d0c5faa0ebef61fe9899dc2
Jeff Kraus <jeff.kraus@atex.com>
2016-08-27 22:08:18 -0400

    add command line argument support to buildstats.sh

--- script/buildstats.sh
+++ script/buildstats.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ -z "$1" ]; then 
+    now_date=`date +%s`
+    days_back=7
+    command_name='buildstats'
+else
+    command_name=$1
+    now_date=$2
+    days_back=$3
+fi
+
 echo "Starting WeeklyStatisticsGenerator from ${BASH_SOURCE%/*} ..."
 mkdir -p ${BASH_SOURCE%/*}/stats
-nohup java -cp ${BASH_SOURCE%/*}/analog-bot-0.6.0-jar-with-dependencies.jar jmk.reddit.weeklystats.WeeklyStatisticsGenerator `date +%s` 1 > ${BASH_SOURCE%/*}/log/nohup.out &
+java -cp ${BASH_SOURCE%/*}/analog-bot-0.6.0-jar-with-dependencies.jar jmk.reddit.weeklystats.WeeklyStatisticsGenerator $command_name $now_date $days_back
