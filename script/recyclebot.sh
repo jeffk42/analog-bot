b7337652a954eec221b4eb192c3bfa79677e5f92
Jeff Kraus <jeff.kraus@atex.com>
2016-09-03 08:51:36 -0400

    avoid double dir switch on recycle

--- script/recyclebot.sh
+++ script/recyclebot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo "Attempting to recycle the AnalogBot process..."
-cd ${BASH_SOURCE%/*}/
-source ./killbot.sh
+source ${BASH_SOURCE%/*}/killbot.sh
 sleep 2
-source ./startbot.sh
+source ${BASH_SOURCE%/*}/startbot.sh
 echo "AnalogBot recycle complete."
-cd -
 exit
